ba21e27e1aae5492ba048d0b31cb54de5c9559c4
Hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-12-15 11:24:50 +0100

    Removing redundant code

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -4,15 +4,6 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[auto_1fr_auto] items-center justify-items-center min-h-screen font-sans bg-gray-50 dark:bg-gray-900">
 
-      {/* Main Content */}
-      <main className="flex flex-col items-center text-center">
-
-      </main>
-
-      {/* Footer */}
-      <footer className="text-sm text-gray-500 dark:text-gray-400">
-
-      </footer>
     </div>
   );
 }
